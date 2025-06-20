d6b08f3e9c155653490dcd1edb92bdc55d7ad754
John.md <106608808+JohnVLN@users.noreply.github.com>
2025-06-20 14:59:17 -0300

    added new components for eath topic

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.scss'
 })
-export class AboutmeComponent {
-
-}
+export class AboutmeComponent {}
