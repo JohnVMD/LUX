d6aa6fe6d50cc490d3396c4a59b72b367532fba5
John.md <106608808+JohnVLN@users.noreply.github.com>
2025-06-20 23:00:09 -0300

    created a landing page

---
+++ src/app/lander/lander.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lander',
+  imports: [],
+  templateUrl: './lander.component.html',
+  styleUrl: './lander.component.scss'
+})
+export class LanderComponent {
+
+}
