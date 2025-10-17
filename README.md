cfde105bec1b6b264a5c1756848517b1feea7983
John.md <106608808+JohnVMD@users.noreply.github.com>
2025-10-17 14:37:10 -0300

    The Great Reset.

--- README.md
+++
@@ -1,5 +0,0 @@
-# Simplicity
-Single page web app made with **Angular 20** framework.
-The objective is to study Angular and learn how to create a good UI/UX design.
-
----
--- src/app/components/aboutme/aboutme.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-aboutme',
-  imports: [],
-  templateUrl: './aboutme.component.html',
-  styleUrl: './aboutme.component.scss'
-})
-export class AboutmeComponent {}
