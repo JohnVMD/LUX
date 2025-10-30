9a102ea32daccdec50d449505069a533ed0e950b
John.md <johnvllanos@gmail.com>
2025-10-29 21:09:27 -0300

    Finally fixed non-existent issue with router, added content to recommended page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Recommend } from './components/recommend/recommend';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home,
-        title: "Home"
-    },
-    {
-        path: 'app-about',
-        component: About,
-        title: 'About'
-    }
+    { path: '', component: Home, title: 'Home'},
+    { path: 'about', component: About, title: 'About'},
+    { path: 'projects', component: Projects, title: 'Projects'},
+    { path: 'recommend', component: Recommend, title: 'Recommendations'}
 ];
---
+++ src/app/components/recommend/recommend.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommend',
+  imports: [],
+  templateUrl: './recommend.html',
+  styleUrl: './recommend.css'
+})
+export class Recommend {
+
+}
