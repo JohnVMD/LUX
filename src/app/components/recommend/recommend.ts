b4788e6a4f64764954a90f15a9abd8b8a9a2a26e
John.md <johnvllanos@gmail.com>
2025-11-04 12:51:29 -0300

    Content

--- src/app/components/recommend/recommend.ts
+++ src/app/components/recommend/recommend.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recommend',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './recommend.html',
   styleUrl: './recommend.css'
 })
