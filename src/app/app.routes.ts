a29ab189c714983e330ff3b5f433ec9da6a48538
JohnCOS <johnvllanos@gmail.com>
2026-02-03 13:43:53 -0500

    we back now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
 import { Recommend } from './components/recommend/recommend';
 
 export const routes: Routes = [
     { path: '', component: Home, title: 'Home'},
-    { path: 'about', component: About, title: 'About'},
     { path: 'skills', component: Skills, title: 'Skills'},
     { path: 'recommend', component: Recommend, title: 'Recommendations'}
 ];
